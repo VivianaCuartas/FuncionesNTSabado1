3f22429eaf8e951e6965b38337da80d1c17f5116
Viviana Cuartas Cardona <vivianacc9714@gmail.com>
2026-02-28 10:43:03 -0500

    Elimino archivos anteriores

--- funcion2.py
+++
@@ -1,9 +0,0 @@
-#Una funcion que crea una lista de n notas
-
-def crear_lista_notas(numeroNotas):
-    import random
-    notas = []
-    for _ in range(numeroNotas):
-        nota = random.randint(1,5)
-        notas.append(nota)
-    return notas
